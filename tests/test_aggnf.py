72c333b0b724eccae093e69cc8a2e571a3d4360f
Jesse Galley <jgalley@hostopia.com>
2018-04-03 23:18:21 -0400

    version 0.2.0, first functional program

--- tests/test_aggnf.py
+++ tests/test_aggnf.py
@@ -11,15 +11,24 @@ Tests for `aggnf` module.
 import unittest
 
 import aggnf
+from aggnf import cli
 
 
 class TestAggnf(unittest.TestCase):
-
     def setUp(self):
         pass
 
-    def test_something(self):
-        assert(aggnf.__version__)
+    def test_counter(self):
+        data = []
+        d = {n: n // 2 for n in range(30)}
+        for k in d:
+            data.append('%s\t%s\n' % (k, d[k]))
+
+        cnt = cli.countfile(in_data=data, sep=None, fieldnum=-1,
+                            ignore_err=False)
+
+        #print(repr(cnt))
+        assert (cnt['5'] == 2)
 
     def tearDown(self):
         pass
